2d7ffa8549e3c19381d9991c16e9516bf743ea4e
DongWoo Ryu <dongwoo@DongWooui-MacBookPro.local>
2026-05-08 20:52:59 +0900

    Add Square favicon

--- school-board/src/app/layout.tsx
+++ school-board/src/app/layout.tsx
@@ -5,6 +5,11 @@ import PopupNotice from "@/components/PopupNotice";
 export const metadata = {
   title: "Square | 청주고등학교 커뮤니티",
   description: "청주고등학교 학생들을 위한 커뮤니티 Square",
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon.svg",
+    apple: "/favicon.svg",
+  },
 };
 
 export const viewport = {
@@ -18,6 +23,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700;900&family=Inter:wght@400;500;600;700;900&display=swap"
           rel="stylesheet"
